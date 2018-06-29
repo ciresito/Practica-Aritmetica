5a4c8bea3e440070245f06514bda4a0a9756264e
Domingo1966 <Domingo1966@github.com>
2018-06-29 11:49:20 -0500

    add Añadiendo calculos

--- Aritmetica.java
+++ Aritmetica.java
@@ -2,10 +2,18 @@ import java.util.Scanner;
 
 public class Aritmetica {
   Scanner tecla = new Scanner(System.in);
+  private double s, r, m, d;
+  
 
   void inicio(){}
   void datos() {}
-  void calculos(){}
+
+  void calculos(){
+  s=prueba.getA()+prueba.getB();
+  r=prueba.getA()-prueba.getB();
+  m=prueba.getA()*prueba.getB();
+  d=prueba.getA()/prueba.getB();
+  }
   void resultados(){}
   public static void main(String []args) {
     Aritmetica a = new Aritmetica();
