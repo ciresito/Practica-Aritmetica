91f9733ca71628489018abefb0df6689aad69f57
cirino silva tovar <cirsilva@yahoo.com>
2018-06-29 09:50:53 -0500

    ahora tenemos un encapsulado segundo commit

--- DosNum.java
+++ DosNum.java
@@ -1,3 +1,9 @@
 public class DosNum {
+  double a, b; 
 
-}
+  void setA(double a) {  this.a = a; }
+  void setB(double b) {  this.b = b; }
+  double getA() {  return a; }
+  double getB() {  return b; }
+  String toString() {  return "a ="+a+" b="+b; }
+}  
