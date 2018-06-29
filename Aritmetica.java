d09d54f52afbe073c8454fb96a9bbc5010695967
Carmen <lcruiz@hotmail.com>
2018-06-29 09:48:54 -0500

    Método datos

--- Aritmetica.java
+++ Aritmetica.java
@@ -2,9 +2,15 @@ import java.util.Scanner;
 
 public class Aritmetica {
   Scanner tecla = new Scanner(System.in);
-
+  DosNum prueba=new DosNum();
   void inicio(){}
-  void datos() {}
+  void datos() {
+  //Metodo para solicitar dos datos
+        System.out.println("Escribir el primer dato:");
+    	prueba.setA=tecla.nextDouble();
+        System.out.println("Escribir el segundo dato:");
+	    prueba.setB=tecla.nextDouble();
+  }
   void calculos(){}
   void resultados(){}
   public static void main(String []args) {
