6dcaeeee5706865ed4de3bf15c787deddeeb9291
cirino silva tovar <cirsilva@yahoo.com>
2018-06-29 09:37:35 -0500

    Commit inicial

---
+++ Aritmetica.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Aritmetica {
+  Scanner tecla = new Scanner(System.in);
+
+  void inicio(){}
+  void datos() {}
+  void calculos(){}
+  void resultados(){}
+  public static void main(String []args) {
+    Aritmetica a = new Aritmetica();
+
+    a.inicio();
+    a.datos();
+    a.calculos();
+    a.resultados();
+  }
+}
